525b474dc822eb9d2535413635cc6e3b2f65440d
Abbas <mohameda@caresoftglobal.com>
2021-06-19 14:36:02 +0530

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const changedElement = screen.getByText(/Changed/i);
+  expect(changedElement).toBeInTheDocument();
+});
